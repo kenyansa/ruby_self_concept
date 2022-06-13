74f3a65c329043ae4d7c69791e690f81bc0977a3
Kenyansa <kenyansajustine@gmail.com>
2022-06-13 12:01:21 +0300

    add implicit self receiver

--- dog.rb
+++ dog.rb
@@ -1,5 +1,5 @@
 class Dog
-    attr_accessor :name
+    attr_accessor :name, :owner
     
     def initialize(name)
         @name = name
@@ -8,6 +8,13 @@ class Dog
     def bark 
         "woof!"
     end
+
+    def get_adopted(owner_name)
+        #self.bark =>calling the Dog#bark method for the dog to bark when adopted
+        bark #with an implicit receiver for self
+        self.owner = owner_name 
+    end
 end
 
 fido = Dog.new("Fido") #creating a new instance of a dog
+fido
