9410a8bf29076f4105672cf45dcbf85f4a10e781
Kenyansa <kenyansajustine@gmail.com>
2022-06-14 05:51:53 +0300

    created class method and variable

--- dog.rb
+++ dog.rb
@@ -12,7 +12,9 @@ class Dog
     def get_adopted(owner_name)
         #self.bark =>calling the Dog#bark method for the dog to bark when adopted
         bark #with an implicit receiver for self
-        self.owner = owner_name 
+        self.owner = owner_name
+        #owner = owner_name => this line is treated as a variable assignment, not a method call
+        #hence, implicit self here does not work
     end
 end
 
