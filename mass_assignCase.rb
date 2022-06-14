6e04d76d57fb2d222cd38c73ee2d0817a9689436
Kenyansa <kenyansajustine@gmail.com>
2022-06-14 23:55:20 +0300

    instantiate a new instance of our own User class

--- mass_assignCase.rb
+++ mass_assignCase.rb
@@ -9,3 +9,12 @@ class User
     @age = age
   end
 end
+#the twitter API may return the below hash:
+twitter_user = { 
+  name: "Sophie", 
+  user_name: "sm_debenedetto", 
+  age: 26, 
+  location: "NY, NY"
+}
+#we now use the hash to instantiate a new instance of our own User class:
+sophie = User.new(twitter_user)
