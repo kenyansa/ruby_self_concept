aed22cb0479f1fcdb07d2ccff3c5348e49313b56
Kenyansa <kenyansajustine@gmail.com>
2022-06-14 23:38:27 +0300

    use double splat oeprator to convert a hash to keyword arguments

--- mass_Assignment.rb
+++ mass_Assignment.rb
@@ -18,4 +18,4 @@ class Person
 end
 #mass assignment
 person_attributes = { name: "Sophie", age: 26 }
-sophie = Person.new(person_attributes) #here, we've created the hash that the method is expecting to accept as an argument, assigned that hash to a variable, and simply passed the variable in to the method as an argument
+sophie = Person.new(**person_attributes) #here, we've created the hash that the method is expecting to accept as an argument, assigned that hash to a variable, and simply passed the variable in to the method as an argument
