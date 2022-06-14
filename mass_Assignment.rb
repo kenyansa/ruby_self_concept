800fd91e6dd010f6b3c093c2dfa7b195c37f95af
Kenyansa <kenyansajustine@gmail.com>
2022-06-14 23:36:44 +0300

    mass-assign variables by creating a hash which will serve as an argument to th method, assign that hash to a varibale, and passing it as an argument

--- mass_Assignment.rb
+++ mass_Assignment.rb
@@ -16,3 +16,6 @@ class Person
     @age = age
   end
 end
+#mass assignment
+person_attributes = { name: "Sophie", age: 26 }
+sophie = Person.new(person_attributes) #here, we've created the hash that the method is expecting to accept as an argument, assigned that hash to a variable, and simply passed the variable in to the method as an argument
