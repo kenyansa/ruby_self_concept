47b88d0101e7fcb477c81a8fc785f44882680181
Kenyansa <kenyansajustine@gmail.com>
2022-06-14 23:13:47 +0300

    demonstrate the problem of using positional arguments without keyword arguments

---
+++ keyword_arguments.rb
@@ -0,0 +1,15 @@
+#with positional argguments:
+def print_name_and_greeting(greeting, name)
+  puts "#{greeting}, #{name}"
+end
+
+print_name_and_greeting("'sup", "Hillary")
+
+#another example of positional arguments:
+def happy_birthday(name, current_age)
+  puts "Happy Birthday, #{name}"
+  current_age += 1
+  puts "You are now #{current_age} years old"
+end
+
+happy_birthday("Justine Kenyansa", 26)
